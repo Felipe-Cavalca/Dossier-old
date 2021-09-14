68ec067740e8f001495d176236b1ff65810ebcee
Felipe-Cavalca <felipe.cavalca@tmh.com.br>
2021-09-13 21:02:36 -0300

    criação de importação de aruqivo css

--- dossier/resources/views/layouts/base.blade.php
+++ dossier/resources/views/layouts/base.blade.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=olá, initial-scale=1.0">
     <title>Dossier</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/index.css')}}" media="screen" />
 </head>
-<body style="background-color: pink;">
+<body>
     <div> isso aqui é a div</div>
     <h1>@yield('txtPrincipal')</h1>
     <div>
