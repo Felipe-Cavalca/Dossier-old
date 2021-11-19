0b0f2b15333e34be0644451586b7675f61c19f26
Felipe-Cavalca <flpsnocvla@gmail.com>
2021-11-19 20:33:11 -0300

    adicionado o pluggin datatables ao sistema
    Signed-off-by: Felipe-Cavalca <flpsnocvla@gmail.com>

--- dossier/resources/views/templates/html.blade.php
+++ dossier/resources/views/templates/html.blade.php
@@ -7,6 +7,7 @@
     <!-- importação de css -->
     <link rel="stylesheet" type="text/css" href="{{asset('bootstrap/css/bootstrap.css')}}" media="screen"/>
     <link rel="stylesheet" type="text/css" href="{{asset('css/components.css')}}" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css" media="screen"/>
     @yield('cssTemplate')
     @yield('css')
     <title>Dossier</title>
@@ -16,6 +17,7 @@
 <!-- importação de js -->
 <script type="text/javascript" src="{{asset('bootstrap/js/bootstrap.js')}}"></script>
 <script type="text/javascript" src="{{asset('jquery/jquery-3.6.0.min.js')}}"></script>
+<script type="text/javascript" src="http://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
 @yield('jsTemplate')
 @yield('js')
 </body>
