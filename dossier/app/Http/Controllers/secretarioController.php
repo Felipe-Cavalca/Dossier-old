ccdfdda90335d5c5474aef7bf9512862c1976bc1
Felipe-Cavalca <flpsnocvla@gmail.com>
2021-11-19 21:15:10 -0300

    adicionado o if para validação do login no secretarioController
    Signed-off-by: Felipe-Cavalca <flpsnocvla@gmail.com>

--- dossier/app/Http/Controllers/secretarioController.php
+++ dossier/app/Http/Controllers/secretarioController.php
@@ -9,21 +9,31 @@ use App\Models\Usuario;
 class secretarioController extends Controller  {
 
     public function arquivos(Request $request){
-        //controle de qual nav será usada
-        $dados['nav'] = Controller::nav();
 
-        return view('secretario/home', $dados);
+        if(Controller::logado($request)){
+            //controle de qual nav será usada
+            $dados['nav'] = Controller::nav();
+
+            return view('secretario/home', $dados);
+        }else{
+            return redirect()->back();
+        }
     }
 
-    public function usuarioList(){
-        //controle de qual nav será usada
-        $dados['nav'] = Controller::nav();
+    public function usuarioList(Request $request){
 
-        $usuarioModel = new Usuario();
-
-        $dados['usuarios'] = $usuarioModel->all();
-
-        return view('secretario/usuarioList', $dados);
+        if(Controller::logado($request)){
+            //controle de qual nav será usada
+            $dados['nav'] = Controller::nav();
+            
+            $usuarioModel = new Usuario();
+            
+            $dados['usuarios'] = $usuarioModel->all();
+            
+            return view('secretario/usuarioList', $dados);
+        }else{
+            return redirect()->back();
+        }
     }
 
 }
