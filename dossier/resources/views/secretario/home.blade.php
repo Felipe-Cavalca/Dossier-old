63c9a2e543f753b783a5e82fbae669a4d3905fd3
Felipe-Cavalca <flpsnocvla@gmail.com>
2021-11-24 20:41:40 -0300

    aujustado designe de listagem de arquivos do secretario
    Signed-off-by: Felipe-Cavalca <flpsnocvla@gmail.com>

--- dossier/resources/views/secretario/home.blade.php
+++ dossier/resources/views/secretario/home.blade.php
@@ -2,13 +2,19 @@
 @extends('navs/secretario')
 
 @section('css')
+<style>
+    iframe{
+        width: 100%;
+        height: 700px;
+    }
+</style>
 @endsection
 
 @section('js')
 @endsection
 
 @section('pagina')
-<a href="/arquivos" target="_blank" >Expandir</a>
+<a href="/arquivos" target="_blank">Expandir</a>
 <hr>
 <iframe src="/arquivos"></iframe>
 @endsection
