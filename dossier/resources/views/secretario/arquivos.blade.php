2c921a3d56109f8e933cfdcefdb7c3527e1b6bbb
Felipe-Cavalca <flpsnocvla@gmail.com>
2021-12-07 17:00:41 -0300

    telas de arquivos movidas para dentro de cards
    Signed-off-by: Felipe-Cavalca <flpsnocvla@gmail.com>

--- dossier/resources/views/secretario/arquivos.blade.php
+++ dossier/resources/views/secretario/arquivos.blade.php
@@ -39,73 +39,85 @@
     </nav>
     <div class="tab-content" id="nav-tabContent">
         <div class="tab-pane fade" id="meus-arquivos">
-            <div class="mt-3">
-                @if (isset($pastaPai))
-                <div class="m-3">
-                    <a href="arquivos?pasta={{$pastaPai}}">
-                        <button class="btn btn-info">Voltar</button>
-                    </a>
+            <div class="card">
+                <div class="card-body">
+                    <div class="mt-3">
+                        @if (isset($pastaPai))
+                        <div class="m-3">
+                            <a href="arquivos?pasta={{$pastaPai}}">
+                                <button class="btn btn-info">Voltar</button>
+                            </a>
+                        </div>
+                        @endif
+                        <table class="table table-striped" id="arquivos">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Downloas/Entrar</th>
+                                    <th scope="col">Excluir</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($arquivos as $arquivo)
+                                <tr>
+                                    <th scope="row">{{$arquivo['nome']}}</th>
+                                    <td>
+                                        @if ($arquivo['tipo'] == 'arquivo')
+                                        <a href="{{ route('download') }}?id={{$arquivo['id']}}" target="_blank">
+                                            <button class="btn btn-secondary">Download</button>
+                                        </a>
+                                        @elseif($arquivo['tipo'] == 'pasta')
+                                        <a href="arquivos?pasta={{$arquivo['id']}}">
+                                            <button class="btn btn-secondary">Entrar</button>
+                                        </a>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <a href="{{ route('deleta') }}?id={{$arquivo['id']}}">
+                                            <button class="btn btn-danger">Excluir</button>
+                                        </a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                @endif
-                <table class="table table-striped" id="arquivos">
-                    <thead>
-                        <tr>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Downloas/Entrar</th>
-                            <th scope="col">Excluir</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($arquivos as $arquivo)
-                        <tr>
-                            <th scope="row">{{$arquivo['nome']}}</th>
-                            <td>
-                                @if ($arquivo['tipo'] == 'arquivo')
-                                <a href="{{ route('download') }}?id={{$arquivo['id']}}" target="_blank">
-                                    <button class="btn btn-secondary">Download</button>
-                                </a>
-                                @elseif($arquivo['tipo'] == 'pasta')
-                                <a href="arquivos?pasta={{$arquivo['id']}}">
-                                    <button class="btn btn-secondary">Entrar</button>
-                                </a>
-                                @endif
-                            </td>
-                            <td>
-                                <a href="{{ route('deleta') }}?id={{$arquivo['id']}}">
-                                    <button class="btn btn-danger">Excluir</button>
-                                </a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
             </div>
         </div>
         <div class="tab-pane fade" id="arquivos-recebidos">B</div>
         <div class="tab-pane fade" id="novo-arquivo">
-            <div class="m-3">
-                <h3>Upload de arquivo</h3>
-                <form action="{{ route('upload') }}" method="post" enctype="multipart/form-data">
-                    @csrf
-                    <div class="input-group mb-3">
-                        <input name="arquivo" type="file" class="form-control" id="arquivo">
-                    </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="m-3">
+                        <h3>Upload de arquivo</h3>
+                        <form action="{{ route('upload') }}" method="post" enctype="multipart/form-data">
+                            @csrf
+                            <div class="input-group mb-3">
+                                <input name="arquivo" type="file" class="form-control" id="arquivo">
+                            </div>
 
-                    <button class="btn btn-primary" type="submit">Enviar Arquivo</button>
-                </form>
+                            <button class="btn btn-primary" type="submit">Enviar Arquivo</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="tab-pane fade" id="nova-pasta">
-            <div class="m-3">
-                <h3>Criação de pasta</h3>
-                <form action="{{ route('criar-pasta') }}" method="post">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="nome" class="form-label">Pasta</label>
-                        <input type="text" name="nome" class="form-control" id="nome">
+            <div class="card">
+                <div class="card-body">
+                    <div class="m-3">
+                        <h3>Criação de pasta</h3>
+                        <form action="{{ route('criar-pasta') }}" method="post">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="nome" class="form-label">Pasta</label>
+                                <input type="text" name="nome" class="form-control" id="nome">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Criar pasta</button>
+                        </form>
                     </div>
-                    <button type="submit" class="btn btn-primary">Criar pasta</button>
-                </form>
+                </div>
             </div>
         </div>
     </div>
